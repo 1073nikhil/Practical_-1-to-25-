53fd15d1257427201ae2e563f9852cecaeb73c0a
1073nikhil <151618293+1073nikhil@users.noreply.github.com>
2023-12-30 01:17:47 +0530

    Create P_Practical21.c

---
+++ P_Practical21.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+    int main()
+        {
+            int arr[] = {1, 2, 3, 4, 5};
+            int *ptr = arr; printf("Original value atThe  *ptr: %d\n", *ptr);
+            ptr++;
+            printf("After incrementing, value at The *ptr: %d\n", *ptr);
+            ptr--;
+            printf("After decrementing, value at The  *ptr: %d\n", *ptr);
+            ptr = arr; 
+            printf("Original value at The  *ptr: %d\n", *ptr);
+            ptr = ptr + 2;
+            printf("After adding 2, value at The *ptr: %d\n", *ptr);
+            ptr = ptr - 1;
+            printf("After subtracting 1, value at The  *ptr: %d\n", *ptr);
+            int *ptr2=arr+3;
+            
+            printf("Value at The *ptr2: %d\n", *ptr2);
+
+            printf("Difference between ptr and ptr2: %ld\n", ptr2 - ptr);
+            return 0;
+        }
