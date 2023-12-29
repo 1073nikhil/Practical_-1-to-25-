98d5618b2e9984d4aaae34c6c173b338f554df5a
1073nikhil <151618293+1073nikhil@users.noreply.github.com>
2023-12-30 00:52:01 +0530

    Create P_Practical04.c

---
+++ P_Practical04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+    void main()
+        {
+	        int n;
+            printf("enter the number That need to be checked:");
+            scanf("%d",n);
+            if(n%2==0)
+                {
+	                  printf("Your number is even");
+                }	
+             else
+                {
+                    printf(" Your Number is odd");
+                }
+        }
