c51ee63a5b109fdd837e5d5216406e9f130dfc92
1073nikhil <151618293+1073nikhil@users.noreply.github.com>
2023-12-30 01:03:43 +0530

    Create P_Practical12.c

---
+++ P_Practical12.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+    typedef int (*ArithmeticOperation)(int, int);
+    int add(int a, int b)
+        {
+            return a + b;
+        }
+    int subtract(int a, int b) 
+        {
+            return a - b;
+        }
+    int multiply(int a, int b) 
+        {
+            return a * b;
+        }
+    int divide(int a, int b) 
+        {
+
+            if (b != 0) 
+                {
+                    return a / b;
+                } 
+            else 
+                {
+                    printf("Error: Division by zero!\n");
+                    return 0;
+                }
+        }
+    void main() 
+        {
+          	ArithmeticOperation operationPtr[4] = {add, subtract, multiply, divide};
+          	int num1, num2;
+          	char operator;
+          	printf("Enter two numbers: ");
+          	scanf("%d %d", &num1, &num2);
+          	printf("Enter arithmetic operation (+, -, *, /): ");
+          	scanf(" %c", &operator);
+          	int result;
+          	switch (operator) 
+              	{
+                  	case '+':
+                  		result = operationPtr[0](num1, num2);
+                  	break;
+                  	case '-':
+                  		result = operationPtr[1](num1, num2);
+                  	break;
+                  	case '*':
+                  		result = operationPtr[2](num1, num2);
+                  	break;
+                  	case '/':
+                  		result = operationPtr[3](num1, num2);
+                  	break;
+                  	default:
+                  	  printf("operator is Inavlid !\n");
+                  	return 1; 
+        	      }
+      	    printf("Result is: %d\n", result);
+        }
