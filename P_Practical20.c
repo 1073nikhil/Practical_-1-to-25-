41495974c65c2a82e690cc5a7c912ea69ccde3ee
1073nikhil <151618293+1073nikhil@users.noreply.github.com>
2023-12-30 01:15:53 +0530

    Create P_Practical20.c

---
+++ P_Practical20.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+        struct Employee 
+                {
+                      int empId;
+                      char empName[50];
+                      float empSalary;
+                };
+        
+        void main() 
+                {
+                        int numEmployees = 3;
+                        struct Employee employees[numEmployees];
+                        for (int i = 0; i < numEmployees; ++i) 
+                                {
+                                      printf("Enter details for The  Employee %d:\n", i + 1);
+                                      printf("ID: ");
+                                      scanf("%d", &employees[i].empId);
+                                      printf("Name: ");
+                                      scanf("%s", employees[i].empName);
+                                      printf("Salary Amount: ");
+                                      scanf("%f", &employees[i].empSalary);
+                                      printf("\n");
+                                }
+                        printf("Database of Employee:\n");
+                        for (int i = 0; i < numEmployees; ++i)
+                                {
+                                      printf("Employee %d\n", i + 1);
+                                      printf("ID: %d\n", employees[i].empId);
+                                      printf("Name: %s\n", employees[i].empName);
+                                      printf("Salary: %.2f\n", employees[i].empSalary);
+                                      printf("\n");
+                                }
+                }
