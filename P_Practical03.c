2f76b5c44ed87293656c074fdc80ce559b722e2a
1073nikhil <151618293+1073nikhil@users.noreply.github.com>
2023-12-30 00:50:24 +0530

    Create P_Practical03.c

---
+++ P_Practical03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+    void main()
+        {
+          	int sum=0;
+          	int n;
+          	printf("enter the number Till which sum is need to be calculated: %d ",n);
+          	
+            for(int i=0;i<=n;i++)
+                {
+          		      sum = sum +i;
+          	    }
+          	printf("sum of the numbers will be :%d",sum);
+        }
